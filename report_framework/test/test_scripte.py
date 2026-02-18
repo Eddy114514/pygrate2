47de43ed86b3b0571ecdb0538a2dbd5499e48b89
Eddy Xu <changbax@andrew.cmu.edu.edu>
2026-02-18 03:37:52 -0500

    add warning for variable leak in list compre

--- report_framework/test/test_scripte.py
+++ report_framework/test/test_scripte.py
@@ -1,14 +1,5 @@
 from test_scripte2 import hello
-d = {"a":1}
-print("a" in d)
-lst = range(5)
+x = "outer"
+ys = [x for x in range(3)]
 
-
-lst.sort(cmp=cmp)
-xyz = cmp
-f = cmp
-def g(x,y):
-  return 1
-a = [xyz(1,2), f(3,4), 5, g(1,2)]
-b = [d.has_key("a"), d.has_key("b")]
 
